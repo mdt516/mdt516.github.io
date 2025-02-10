750ac48ed5f827a85b348c44973dc9dbfd8c1235
mdt516 <mtindall516@gmail.com>
2025-02-09 22:04:52 -0600

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Portfolio Website
+This is where all the files are for my portfolio website.
+
+Right now, there's not much. I'd like to add functionality for "open in new tab" on the buttons. not sure how to do that. Also don't really know exactly how this is hosted (if at all). Anyway, it's cool to have this!
---
+++ script.js
@@ -0,0 +1,12 @@
+const githubButton = document.getElementById("github")
+const liButton = document.getElementById("linkedin")
+
+githubButton.addEventListener("click", function()
+{
+    window.location.href = "https://github.com/mdt516";
+})
+
+liButton.addEventListener("click", function()
+{
+    window.location.href = "https://www.linkedin.com/in/matt-tindall-7698972ba/";
+})
