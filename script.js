f9adc4c0969052cd5b72382bc03f9dfaca281d47
mdt516 <mtindall516@gmail.com>
2025-08-31 22:12:55 -0500

    Added calculator project and link buttons to the demo and source code

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const githubButton = document.getElementById("github")
 const liButton = document.getElementById("linkedin")
+const calcProj = document.getElementById("calc")
+const weblink = document.getElementById("weblink")
+const calcDemo = document.getElementById("calcPr")
+
 
 githubButton.addEventListener("click", function()
 {
@@ -9,4 +13,19 @@ githubButton.addEventListener("click", function()
 liButton.addEventListener("click", function()
 {
     window.location.href = "https://www.linkedin.com/in/matt-tindall-7698972ba/";
+})
+
+calcProj.addEventListener("click", function()
+{
+    window.location.href = "https://github.com/mdt516/calculator";
+})
+
+weblink.addEventListener("click", function()
+{
+    window.location.href = "https://github.com/mdt516/mdt516.github.io";
+})
+
+calcDemo.addEventListener("click", function()
+{
+    window.location.href = "https://mdt516.github.io/calculator"
 })
